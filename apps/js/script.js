c7028de45e46a33719e7066bab6014579ea4fa43
Maggie Mouat <31249766+mbmouat@users.noreply.github.com>
2017-12-05 18:11:15 -0500

    Add files via upload

---
+++ apps/js/script.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+
+  console.log("loaded");
+  $("#las-vegas").hide();
+  $("#texas").hide();
+
+    $("#texas-btn").click(function(){
+      console.log("clicked");
+        $("#gun-control").hide();
+        $("#las-vegas").hide();
+        $("#texas").show();
+
+      });
+
+      $("#las-vegas-btn").click(function(){
+        console.log("clicked");
+          $("#gun-control").hide();
+          $("#texas").hide();
+          $("#las-vegas").show();
+
+        });
+
+        // new york times
+        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += '?' + $.param({
+          'api-key': "10bda7bf71614f6cb64801a21b68dc16",
+          'q': "Mass Shootings"
+        });
+        $.ajax({
+          url: url,
+          method: 'GET',
+        }).done(function(result) {
+          console.log(result);
+   //parse of the json from nytimes
+      var html = "";
+       var byline = [];
+      var docs = [];
+      var tempPath = result["response"]["docs"];
+      console.log(tempPath);
+
+     for (var i = 0, len = tempPath.length; i < len; ++i) {
+       console.log(i);
+
+       byline.push(tempPath[i]["headline"]);
+       byline.push(tempPath[i]["snippet"]);
+
+      html += '<div class="article"><a href="https://www.nytimes.com/" (\''  + tempPath[i]["uri"] + '\')">' + tempPath[i]["headline"]["main"] + '</a></div>'
+      \xa0 - 
+      html += '<div class="description"><a href="https://www.nytimes.com/" (\')' + tempPath[i]["uri"] + '\')">' + tempPath[i]["snippet"] + '</a></div>'
+     }
+
+      $("#result").html(html);
+
+ }).fail(function(err) {
+   throw err;
+
+
+ });
+ google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawStacked);
+
+function drawStacked() {
+      var data = google.visualization.arrayToDataTable([
+        ['Month', 'Killed', 'Wounded'],
+        ['Jan', 62, 138],
+        ['Feb', 44, 109],
+        ['March', 58, 92],
+        ['April', 46, 176],
+        ['May', 30, 100],
+        ['June', 60, 149],
+        ['July', 42, 179],
+        ['Aug', 34, 154],
+        ['Sept', 32, 126],
+        ['Oct', 94,548],
+        ['Nov', 63,128],
+      ]);
+
+      var options = {
+        title: 'The numbers behind mass shootings',
+        chartArea: {width: '50%'},
+        isStacked: true,
+        hAxis: {
+          title: 'Killed and Wounded',
+          minValue: 0,
+        },
+        vAxis: {
+          title: 'Months'
+        }
+      };
+      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    }
+});
---
+++ apps/twitter/php/texas.php
@@ -0,0 +1,60 @@
+<head>
+<center><h1>#SutherlandSprings</h1></center>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<link href="../php/styles.css" rel="stylesheet">
+</head>
+
+<?php
+ini_set('display_errors', 1);
+require_once('../TwitterAPIExchange.php');
+
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' =>"2385620364-UIWjpDs4MrkPoclLfy0n6wPVVnBe0lcJwtsCW6a",
+    'oauth_access_token_secret' =>"p4GvIAFkDLnQOwGVAQYOY63qxB5mAERt2jztU67CowZQe",
+    'consumer_key' => "jAztCOrHlTt9N1ZX1VM5ky5zn",
+    'consumer_secret' => "vC6j5vVVC5I0PfAEb3ha28vvkNIR5UDwwHcXiTGicIR18Q3S2g"
+);
+
+/** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
+//$url = 'https://api.twitter.com/1.1/blocks/create.json';
+$requestMethod = 'GET';
+
+/** POST fields required by the URL above. See relevant docs as above **/
+$postfields = array(
+    'screen_name' => 'usernameToBlock',
+    'skip_status' => '1'
+);
+
+/** Perform a POST request and echo the response **/
+//$twitter = new TwitterAPIExchange($settings);
+//echo $twitter->buildOauth($url, $requestMethod)
+             //->setPostfields($postfields)
+        //  ->performRequest();
+
+/** Perform a GET request and echo the response **/
+/** Note: Set the GET field BEFORE calling buildOauth(); **/
+$url = 'https://api.twitter.com/1.1/search/tweets.json';
+$getfield = '?q=sutherlandsprings';
+$requestMethod = 'GET';
+$twitter = new TwitterAPIExchange($settings);
+$tweetData= json_decode ($twitter->setGetfield($getfield)
+             ->buildOauth($url, $requestMethod)
+             ->performRequest(),$assoc=TRUE);
+
+
+             foreach($tweetData['statuses'] as $index => $items){
+               $userArray = $items['user'];
+               echo '<div class="twee">';
+               echo '<div class="avatar">' . '<a href="http://twitter.com/' . $userArray['screen_name'] . '"><img src="' . $userArray['profile_image_url'] . '"></a></div>';
+               echo $userArray['screen_name'] . "<br />";
+               echo '<div class="tweet">' . $items['text']. "</div>";
+               echo '</div>';
+             }
+?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="../../js/tweetLinkIt.js"></script>
+<script>
+$('.tweet').tweetLinkify();
+</script>
